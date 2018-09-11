1c55d9a00188d272b864346431c956ccf0cc8db1
Michael Bolin <bolinfest@gmail.com>
2018-09-10 22:19:11 -0700

    More comments after trying CString::from_vec_unchecked().

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,15 @@ pub extern "C" fn kprobe__sys_clone(_ctx: *mut u8) -> i32 {
   let BPF_FUNC_trace_printk = unsafe {
     // ::std::os::raw::c_char seems more appropriate than u8,
     // but converting msg using CStr::from_bytes_with_nul() seems to
-    // mess things up. Perhaps CString::from_vec_unchecked() could
-    // be used with as_ptr() to achieve this instead?
+    // mess things up.
+    //
+    // I tried using a combination of CString::from_vec_unchecked() and
+    // as_ptr(), but I got this error when trying to generate the BPF
+    // bytecode:
+    //
+    // error: <unknown>:0:0: in function kprobe__sys_clone i32 (i8*): only small returns supported
+    //
+    // Perhaps this is because std::os::raw::c_char is i8 instead of u8?
     transmute::<u64, fn(*const u8, i32) -> i32>(6)
   };
 
