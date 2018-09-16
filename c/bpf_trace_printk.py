ee5ac46c292728f3b31071a886c207bf491a0c0d
Michael Bolin <bolinfest@gmail.com>
2018-09-15 18:32:28 -0700

    Improve print_list_of_instructions() debug function.
    The idea is that we can change the `bpf_text`
    in `bpf_trace_printk.py` to whatever we like and then
    use this to dump the bytecode.
    This is basically the same as what `llvm-objdump -S` does,
    though it gives us a little more control over the
    output.
    
    Here is the new output:
    
    ```
    $ python bpf_trace_printk.py
    0 180100006f6d2072 # r1 = 0x0a74737572206d6f
    1 000000007573740a
    2 7b1ae8ff00000000 # *(u64 *) (r10 + -24) = r1
    3 1801000068656c6c # r1 = 0x7266206f6c6c6568
    4 000000006f206672
    5 7b1ae0ff00000000 # *(u64 *) (r10 + -32) = r1
    6 b701000000000000 # r1 = 0
    7 731af0ff00000000 # *(u8 *) (r10 + -16) = r1
    8 bfa1000000000000 # r1 = r10
    9 07010000e0ffffff # r1 += -32
    10 b702000011000000 # r2 = 17
    11 8500000006000000 # call bpf_trace_printk()
    12 b700000000000000 # r0 = 0
    13 9500000000000000 # exit
    ```
    
    Admittedly, it might be better to simply use the bcc tools
    to dump the ELF file and then just use `llvm-objdump`, but
    I haven't found the magic incantation in libbpf or elsewhere
    to do that yet.

--- c/bpf_trace_printk.py
+++ c/bpf_trace_printk.py
@@ -1,6 +1,5 @@
 from bcc import ArgString, BPF
-from binascii import hexlify
-import struct
+from debug import print_bpf_insn_struct, print_list_of_instructions
 
 # define BPF program
 bpf_text = """
@@ -18,44 +17,5 @@ int trace_entry(struct pt_regs *ctx, int dfd, const char __user *filename)
 b = BPF(text=bpf_text)
 bytecode = b.dump_func("trace_entry")
 
-
-def print_list_of_instructions():
-    for index, b_offset in enumerate(xrange(0, len(bytecode), 8)):
-        instruction = bytecode[b_offset : b_offset + 8]
-        print("%2d %s" % (index, hexlify(instruction)))
-
-
-bpf_insn_template = """\
-  ((struct bpf_insn) {
-      .code    = 0x%s,
-      .dst_reg = BPF_REG_%d,
-      .src_reg = BPF_REG_%d,
-      .off     = %d,
-      .imm     = %d,
-  }),"""
-
-
-def print_bpf_insn_struct():
-    """Prints the eBPF bytecode as the equivalent bpf_insn[] in C."""
-    print("struct bpf_insn prog[] = {")
-    for b_offset in xrange(0, len(bytecode), 8):
-        instruction = bytecode[b_offset : b_offset + 8]
-        # u8
-        opcode = instruction[0]
-
-        src_and_dst = struct.unpack("B", instruction[1])[0]
-        # The low-order nibble is dst.
-        dst_reg = src_and_dst & 0x0F
-        # The high-order nibble is src.
-        src_reg = (src_and_dst & 0xF0) >> 4
-
-        # s16
-        offset = struct.unpack("h", instruction[2:4])[0]
-        # s32
-        imm = struct.unpack("i", instruction[4:8])[0]
-        print(bpf_insn_template % (hexlify(opcode), dst_reg, src_reg, offset, imm))
-    print("};")
-
-
-# print_list_of_instructions()
-print_bpf_insn_struct()
+print_list_of_instructions(bytecode)
+# print_bpf_insn_struct(bytecode)
