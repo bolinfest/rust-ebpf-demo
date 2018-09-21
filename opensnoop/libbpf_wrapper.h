7519bd568bc3dbd27f7e236ca7f8f1f2560f8e6b
Michael Bolin <bolinfest@gmail.com>
2018-09-21 14:43:43 -0700

    stdint.h appears more appropriate than inttypes.h.

--- opensnoop/libbpf_wrapper.h
+++ opensnoop/libbpf_wrapper.h
@@ -1,7 +1,7 @@
 #ifndef LIBBPF_WRAPPER_H
 #define LIBBPF_WRAPPER_H
 
-#include <inttypes.h>  // <bcc/libbpf.h> needs this for uint64_t and uint32_t.
+#include <stdint.h>    // <bcc/libbpf.h> needs this for uint64_t and uint32_t.
 #include <sys/types.h> // <bcc/libbpf.h> needs this for size_t and pid_t.
 
 #include <bcc/libbpf.h>
